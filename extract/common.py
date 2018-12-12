e6c1f47d15e0eed05b5381f47e56f99a67f9299b
redshiftzero <jen@freedom.press>
2018-12-11 21:48:35 -0800

    AK: Add extraction function for bill text in <pre> tags

--- extract/common.py
+++ extract/common.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 from .utils import pdfdata_to_text, text_after_line_numbers
 
 
@@ -7,3 +9,22 @@ def extract_simple_pdf(data, metadata):
 
 def extract_line_numbered_pdf(data, metadata):
     return text_after_line_numbers(pdfdata_to_text(data))
+
+
+def extract_pre_tag_html(data, metadata):
+    """
+    Many states that provide bill text on HTML webpages (e.g. AK, FL)
+    have the text inside <pre> tags (for preformatted text).
+    """
+
+    html_document = BeautifulSoup(data, "lxml")
+    pre_tags = html_document.find_all("pre")
+
+    # To ensure that we exit non-zero if there are multiple <pre> tags
+    # on the page, raise an exception: this means that the extraction
+    # code needs to be updated.
+    assert len(pre_tags) == 1
+
+    text_inside_pre_tag = pre_tags[0].text
+
+    return text_after_line_numbers(text_inside_pre_tag)
