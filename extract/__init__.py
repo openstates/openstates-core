3765a18a5103c1bbec7082acb6f6ebf8124c2966
redshiftzero <jen@freedom.press>
2018-12-11 20:59:44 -0800

    AL: use extract_line_numbered_pdf
    There's a bit of boilerplate at the top of the PDFs but in the
    interest of not being overly aggressive since some of this text
    might be useful, leaving it in.

--- extract/__init__.py
+++ extract/__init__.py
@@ -2,7 +2,7 @@ from .utils import jid_to_abbr
 from .common import extract_simple_pdf, extract_line_numbered_pdf
 
 
-CONVERSION_FUNCTIONS = {"nc": extract_line_numbered_pdf}
+CONVERSION_FUNCTIONS = {"al": extract_line_numbered_pdf, "nc": extract_line_numbered_pdf}
 
 
 def extract_text(data, metadata):
