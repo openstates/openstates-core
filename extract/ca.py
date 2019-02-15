3ce32570a5d12e8009d83a1bda7e642a33df83ff
Jeremy B. Merrill <jmerrill@qz.com>
2019-02-15 16:46:39 -0500

    whitespace, sorry

--- extract/ca.py
+++ extract/ca.py
@@ -3,14 +3,12 @@ from .common import (
 )
 
 def handle_california(data, metadata):
-	if metadata["media_type"] == "text/html":
-		return extractor_for_element_by_xpath('.//div[@id="bill"] | .//span[@class="Resolution"]')(data, metadata)
-	elif metadata["media_type"] == "application/pdf":
-		# return extract_simple_pdf(data, metadata)
-
-		# according to James, Calif. publishes bills as both text and HTML
-		# so we don't *need* to extract from PDF.
-		pass
-	else:
-		print("unexpected media type in california", metadata["media_type"])
-		assert False
+    if metadata["media_type"] == "text/html":
+        return extractor_for_element_by_xpath('.//div[@id="bill"] | .//span[@class="Resolution"]')(data, metadata)
+    elif metadata["media_type"] == "application/pdf":
+        # according to James, Calif. publishes bills as both text and HTML
+        # so we don't *need* to extract from PDF.
+        pass
+    else:
+        print("unexpected media type in california", metadata["media_type"])
+        assert False
--- extract/de.py
+++ extract/de.py
@@ -9,24 +9,24 @@ from .common import (
 
 
 def handle_delaware(data, metadata):
-	if metadata["media_type"] == "text/html" and 'HCR' not in metadata["title"]:
-		return extractor_for_elements_by_xpath('/html/body/div[2] | /html/body/div[3]')(data, metadata)
-	elif metadata["media_type"] == "application/pdf"  and 'HCR' not in metadata["title"]:
-		# return extract_simple_pdf(data, metadata)
+    if metadata["media_type"] == "text/html" and 'HCR' not in metadata["title"]:
+        return extractor_for_elements_by_xpath('/html/body/div[2] | /html/body/div[3]')(data, metadata)
+    elif metadata["media_type"] == "application/pdf"  and 'HCR' not in metadata["title"]:
+        # return extract_simple_pdf(data, metadata)
 
-		# Del., like many states, appears to publish all bills as both text and HTML
-		# so we don't *need* to extract from PDF.
-		pass
-	elif metadata["media_type"] == "application/pdf"  and 'HCR' in metadata["title"]:
-		# haha, but Delaware does NOT appear to publish concurrent resolutions (honoring people and recognizing the contributions of general aviation, etc.)
-		# as HTML. Just PDF and MSWord. 
-		# so we handle these here. 
-		return extract_line_numbered_pdf(data, metadata)
-	elif metadata["media_type"] == "application/msword":
-		# haha, but Delaware does NOT appear to publish concurrent resolutions (honoring people and recognizing the contributions of general aviation, etc.)
-		# as HTML. Just PDF and MSWord. 
-		# so we handle these as PDFs above (and ignore docx.)
-		pass 
-	else:
-		print("unexpected media type in delaware", metadata["media_type"])
-		assert False
+        # Del., like many states, appears to publish all bills as both text and HTML
+        # so we don't *need* to extract from PDF.
+        pass
+    elif metadata["media_type"] == "application/pdf"  and 'HCR' in metadata["title"]:
+        # haha, but Delaware does NOT appear to publish concurrent resolutions (honoring people and recognizing the contributions of general aviation, etc.)
+        # as HTML. Just PDF and MSWord. 
+        # so we handle these here. 
+        return extract_line_numbered_pdf(data, metadata)
+    elif metadata["media_type"] == "application/msword":
+        # haha, but Delaware does NOT appear to publish concurrent resolutions (honoring people and recognizing the contributions of general aviation, etc.)
+        # as HTML. Just PDF and MSWord. 
+        # so we handle these as PDFs above (and ignore docx.)
+        pass 
+    else:
+        print("unexpected media type in delaware", metadata["media_type"])
+        assert False
