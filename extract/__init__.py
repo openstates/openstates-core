395b288a7b746f28d6718098ed91281aca554ce7
James Turk <dev@jamesturk.net>
2019-11-18 13:01:59 -0500

    Iowa and some housekeeping

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,12 +1,12 @@
 from .utils import jid_to_abbr
 from .common import (
-    extract_simple_pdf,
+    # extract_simple_pdf,
     extract_line_numbered_pdf,
     extract_pre_tag_html,
     extract_sometimes_numbered_pdf,
     extract_from_p_tags_html,
     extractor_for_elements_by_class,
-    extractor_for_element_by_id,
+    # extractor_for_element_by_id,
     extractor_for_element_by_xpath,
     extract_from_code_tags_html,
 )
@@ -17,16 +17,13 @@ CONVERSION_FUNCTIONS = {
     "ak": extract_pre_tag_html,
     "az": extractor_for_elements_by_class("WordSection2"),
     "ar": extract_sometimes_numbered_pdf,
-    "co": extract_sometimes_numbered_pdf,
-    "ct": extract_from_p_tags_html,
-    "id": extract_line_numbered_pdf,
-    "il": extract_from_code_tags_html,
-    "nc": extract_line_numbered_pdf,
     "ca": {
         "text/html": extractor_for_element_by_xpath(
             './/div[@id="bill"] | .//span[@class="Resolution"]'
         )
     },
+    "co": extract_sometimes_numbered_pdf,
+    "ct": extract_from_p_tags_html,
     "de": handle_delaware,
     "fl": {"text/html": extract_pre_tag_html, "application/pdf": extract_line_numbered_pdf},
     "hi": {
@@ -34,6 +31,10 @@ CONVERSION_FUNCTIONS = {
             './/*[@class="Section2"] | .//*[@class="WordSection2"]'
         )
     },
+    "ia": {"application/pdf": extract_line_numbered_pdf},
+    "id": extract_line_numbered_pdf,
+    "il": extract_from_code_tags_html,
+    "nc": extract_line_numbered_pdf,
     "wi": {"application/pdf": extract_sometimes_numbered_pdf},
     "wy": extract_sometimes_numbered_pdf,
 }
