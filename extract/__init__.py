3c1bccd001c15e54e5007442f82c075b595b8679
James Turk <dev@jamesturk.net>
2019-11-19 17:52:07 -0500

    Mass.

--- extract/__init__.py
+++ extract/__init__.py
@@ -48,6 +48,7 @@ CONVERSION_FUNCTIONS = {
     "id": {"application/pdf": extract_line_numbered_pdf},
     "il": {"text/html": extract_from_code_tags_html},
     "la": {"application/pdf": extract_sometimes_numbered_pdf},
+    "ma": {"application/pdf": extract_line_numbered_pdf},
     "md": {"application/pdf": extract_line_numbered_pdf},
     "me": {
         "text/html": extractor_for_elements_by_class("billtextbody"),
