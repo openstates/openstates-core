da857069118d83d6f2196e287b167419cd5f73d8
James Turk <dev@jamesturk.net>
2020-02-05 11:05:18 -0500

    add lookup by name

--- openstates_metadata/__init__.py
+++ openstates_metadata/__init__.py
@@ -1,8 +1,10 @@
-from .data import STATES_BY_ABBR, STATES_BY_JID
+from .data import STATES_BY_ABBR, STATES_BY_JID, STATES_BY_NAME
 
 
-def lookup(*, abbr=None, jurisdiction_id=None):
+def lookup(*, abbr=None, jurisdiction_id=None, name=None):
     if abbr:
         return STATES_BY_ABBR[abbr.upper()]
+    if name:
+        return STATES_BY_NAME[name.lower()]
     if jurisdiction_id:
         return STATES_BY_JID[jurisdiction_id]
--- openstates_metadata/data/__init__.py
+++ openstates_metadata/data/__init__.py
@@ -109,3 +109,4 @@ STATES = [
 
 STATES_BY_ABBR = {s.abbr: s for s in STATES}
 STATES_BY_JID = {s.jurisdiction_id: s for s in STATES}
+STATES_BY_NAME = {s.name.lower(): s for s in STATES}
