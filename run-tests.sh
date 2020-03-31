7b0986a6d79e95e949939aace626e5d9817950cc
James Turk <dev@jamesturk.net>
2020-03-31 16:45:10 -0400

    add exclusion for coverage tool

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-export PYTHONPATH=.; poetry run pytest --cov openstates_core --cov-report html --ds=openstates_core.test_settings --cov-config=.coveragerc $@
-coverage report -m
+export PYTHONPATH=.; poetry run pytest --cov openstates_core --cov-report html --ds=openstates_core.test_settings $@
+poetry run coverage report -m
