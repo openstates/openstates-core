2eb94af7e1f9daefb00dcc969206e459b80fc050
James Turk <dev@jamesturk.net>
2020-08-03 12:03:00 -0400

    more tests for initdb idempotentcy

--- openstates/cli/tests/test_initdb.py
+++ openstates/cli/tests/test_initdb.py
@@ -2,6 +2,7 @@ import pytest
 from openstates.data.models import Jurisdiction, Division, Organization
 from openstates.cli.initdb import create_division, create_chamber
 from openstates_metadata import lookup
+from django.db.utils import IntegrityError
 
 
 @pytest.mark.django_db
@@ -40,3 +41,53 @@ def test_create_chamber_basic():
     assert org.name == nc.lower.name
     assert org.id == nc.lower.organization_id
     assert org.posts.count() == 120
+
+
+@pytest.mark.django_db
+def test_create_chamber_duplicate_idempotent():
+    nc = lookup(abbr="NC")
+
+    juris = Jurisdiction.objects.create(
+        id=nc.jurisdiction_id, name=nc.name, division=None
+    )
+    leg = Organization.objects.create(
+        id=nc.legislature_organization_id,
+        name=nc.legislature_name,
+        classification="legislature",
+        jurisdiction=juris,
+    )
+
+    # second call, identical to first, should be idempotent
+    create_chamber(juris, leg, nc.lower)
+    create_chamber(juris, leg, nc.lower)
+
+    assert Organization.objects.filter(classification="lower").count() == 1
+
+    # ensure the org and posts were created
+    org = Organization.objects.get(classification="lower")
+    assert org.name == nc.lower.name
+    assert org.id == nc.lower.organization_id
+    assert org.posts.count() == 120
+
+
+@pytest.mark.django_db
+def test_create_chamber_duplicate_with_changes():
+    nc = lookup(abbr="NC")
+
+    juris = Jurisdiction.objects.create(
+        id=nc.jurisdiction_id, name=nc.name, division=None
+    )
+    leg = Organization.objects.create(
+        id=nc.legislature_organization_id,
+        name=nc.legislature_name,
+        classification="legislature",
+        jurisdiction=juris,
+    )
+
+    create_chamber(juris, leg, nc.lower)
+    # second call, but lower chamber name has been changed
+    nc.lower.name = "Ronald McDonald House of Clowns"
+    with pytest.raises(IntegrityError):
+        create_chamber(juris, leg, nc.lower)  # unsupported, should definitely be loud
+
+    assert Organization.objects.filter(classification="lower").count() == 1
