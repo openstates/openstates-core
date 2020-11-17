80ce66047a061a653bc9a2355945bacf70d6e9ec
James Turk <dev@jamesturk.net>
2020-11-17 15:42:27 -0500

    remove OrderVoteEvent in favor of VoteEvent.order

--- openstates/scrape/__init__.py
+++ openstates/scrape/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 from .jurisdiction import Jurisdiction, JurisdictionScraper
 from .popolo import Membership, Organization, Person, Post
-from .vote_event import VoteEvent, OrderVoteEvent
+from .vote_event import VoteEvent
 from .bill import Bill
 from .event import Event
 from .base import Scraper, BaseBillScraper
