7214c07eb23b566b168aaf7639ec17a07a866df0
James Turk <dev@jamesturk.net>
2021-04-06 14:18:19 -0400

    parametrize test

--- openstates/utils/tests/test_transformers.py
+++ openstates/utils/tests/test_transformers.py
@@ -1,8 +1,16 @@
+import pytest
 from openstates.utils.transformers import fix_bill_id
 
 
-def test_fix_bill_id():
-    assert fix_bill_id("HB1") == "HB 1"
-    assert fix_bill_id("HB 0001") == "HB 1"
-    assert fix_bill_id("SJRA") == "SJR A"
-    assert fix_bill_id("hb   1") == "HB 1"
+@pytest.mark.parametrize(
+    "orig,exp",
+    [
+        ("HB1", "HB 1"),
+        ("   HB1  ", "HB 1"),
+        ("HB 0001", "HB 1"),
+        ("hb    1", "HB 1"),
+        ("SJRA", "SJR A"),
+    ],
+)
+def test_fix_bill_id(orig, exp):
+    assert fix_bill_id(orig) == exp
