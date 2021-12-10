357f0da2025dffea40c0d35e2a3e7a074aabd6a4
James Turk <dev@jamesturk.net>
2021-12-09 21:10:14 -0500

    --*only flags to os-scrape

--- openstates/cli/scrape.py
+++ openstates/cli/scrape.py
@@ -7,14 +7,20 @@ import click
 @click.command()
 @click.argument("abbr")
 @click.argument("scraper_type")
-def main(abbr: str, scraper_type: str):
+@click.option("--scrape-only/--no-scrape-only", help="Only perform scrape.")
+@click.option("--merge-only/--no-merge-only", help="Only perform merge.")
+def main(abbr: str, scraper_type: str, scrape_only: bool, merge_only: bool):
     output_dir = Path(f"_scrapes/{abbr}/{scraper_type}")
-    try:
-        scrape([f"scrapers_next.{abbr}.{scraper_type}", "-o", output_dir, "--rmdir"])
-    except SystemExit as e:
-        if e.code != 0:
-            raise
-    merge([abbr, str(output_dir)])
+    if not merge_only:
+        try:
+            scrape(
+                [f"scrapers_next.{abbr}.{scraper_type}", "-o", output_dir, "--rmdir"]
+            )
+        except SystemExit as e:
+            if e.code != 0:
+                raise
+    if not scrape_only:
+        merge([abbr, str(output_dir)])
 
 
 if __name__ == "__main__":
