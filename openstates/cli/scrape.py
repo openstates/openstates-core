1d63c67ff99d02aa7cfc8c42a90974c8defedf38
James Turk <dev@jamesturk.net>
2021-12-14 14:57:40 -0500

    add --fastmode to os-scrape

--- openstates/cli/scrape.py
+++ openstates/cli/scrape.py
@@ -7,15 +7,28 @@ import click
 @click.command()
 @click.argument("abbr")
 @click.argument("scraper_type")
-@click.option("--scrape-only/--no-scrape-only", help="Only perform scrape.")
-@click.option("--merge-only/--no-merge-only", help="Only perform merge.")
-def main(abbr: str, scraper_type: str, scrape_only: bool, merge_only: bool):
+@click.option("--scrape-only", help="Only perform scrape.", is_flag=True)
+@click.option("--merge-only", help="Only perform merge.", is_flag=True)
+@click.option(
+    "--fastmode",
+    help="Use a cache to avoid making unnecessary requests while developing.",
+    is_flag=True,
+)
+def main(
+    abbr: str, scraper_type: str, scrape_only: bool, merge_only: bool, fastmode: bool
+):
     output_dir = Path(f"_scrapes/{abbr}/{scraper_type}")
     if not merge_only:
+        args = [
+            f"scrapers_next.{abbr}.{scraper_type}",
+            "-o",
+            output_dir,
+            "--rmdir",
+        ]
+        if fastmode:
+            args.append("--fastmode")
         try:
-            scrape(
-                [f"scrapers_next.{abbr}.{scraper_type}", "-o", output_dir, "--rmdir"]
-            )
+            scrape(args)
         except SystemExit as e:
             if e.code != 0:
                 raise
