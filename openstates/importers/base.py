7a47d2aeee55b51cd99451d18eff8ee0ab373a20
Sogo Ogundowole <dowolebolu@gmail.com>
2022-12-29 19:18:13 +0100

    chore: temp log for import errors

--- openstates/importers/base.py
+++ openstates/importers/base.py
@@ -298,7 +298,9 @@ class BaseImporter:
         for json_id, data in self._prepare_imports(data_items):
             obj_id, what = self.import_item(data)
             if not obj_id or not what:
-                self.logger.warning(f"Skipped {data} because it did not have an associated ID or type")
+                self.logger.warning(
+                    f"Skipped {data} because it did not have an associated ID or type"
+                )
                 continue
             self.json_to_db_id[json_id] = obj_id
             record["records"][what].append(obj_id)
@@ -492,6 +494,9 @@ class BaseImporter:
             try:
                 Subtype.objects.bulk_create(subobjects)
             except Exception as e:
+                self.error("Error bulk creating related objects: {}".format(e))
+                self.error(f"Related objects: {subobjects}")
+                self.error(f"Subtype: {Subtype}")
                 raise DataImportError(
                     "{} while importing {} as {}".format(e, subobjects, Subtype)
                 )
