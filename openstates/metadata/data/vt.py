f5354997064074cfd8c0308fc6a6ea9e3e4598f1
John Seekins <john@civiceagle.com>
2023-01-10 15:27:15 -0700

    another district for VT
    Signed-off-by: John Seekins <john@civiceagle.com>

--- openstates/metadata/data/vt.py
+++ openstates/metadata/data/vt.py
@@ -553,6 +553,12 @@ VT = State(
                 1,
             ),
             District(
+                "Windsor-Windham",
+                "lower",
+                "ocd-division/country:us/state:vt/sldl:windsor-windham",
+                1,
+            ),
+            District(
                 "Rutland-Windsor-1",
                 "lower",
                 "ocd-division/country:us/state:vt/sldl:rutland-windsor-1",
