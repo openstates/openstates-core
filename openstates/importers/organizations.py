9354ec7a2370f9f8f9fc996334db56ec374e7319
NewAgeAirbender <34139325+NewAgeAirbender@users.noreply.github.com>
2024-03-25 16:39:44 -0500

    update organization importer limit_spec

--- openstates/importers/organizations.py
+++ openstates/importers/organizations.py
@@ -14,5 +14,5 @@ class OrganizationImporter(BaseImporter):
 
         name = spec.pop("name", None)
         if name:
-            return Q(**spec) & Q(name=name)
+            return Q(**spec) & (Q(name__iexact=name) | Q(other_names__name__iexact=name))
         return spec
