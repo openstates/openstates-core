ccd26c4245b0e9eadb8a8454ef0cf4bb4a700515
alexobaseki <alex@pluralpolicy.com>
2025-02-27 16:31:22 -0500

    Remove subprocess

--- openstates/cli/people_repo_update.py
+++ openstates/cli/people_repo_update.py
@@ -1,4 +1,3 @@
-import argparse
 import datetime
 import os
 import typing
@@ -6,11 +5,14 @@ import typing
 import requests
 import logging
 import shutil
-import subprocess
 
 from pathlib import Path
 
+import click
 import git
+
+from ..cli.committees import to_database as committee_to_database
+from ..cli.people import to_database as people_to_database
 
 logger = logging.getLogger(__name__)
 
@@ -60,35 +62,6 @@ def clone_people_repo() -> None:
     shutil.copytree(source_data_dir, destination_data_dir)
 
 
-def opts() -> tuple[argparse.Namespace, list[str]]:
-    parser = argparse.ArgumentParser(
-        description="Trigger Openstates people and committees to-database",
-    )
-    parser.add_argument(
-        "--force-ingest",
-        "-f",
-        action="store_true",
-        help="Optionally set to True to force ingestion regardless of last commit",
-    )
-    parser.add_argument(
-        "--purge",
-        "-p",
-        action="store_true",
-        help="Set to True to purge old data from database",
-    )
-    parser.add_argument(
-        "--people",
-        action="store_true",
-        help="Set to True to ingest only people data",
-    )
-    parser.add_argument(
-        "--committees",
-        action="store_true",
-        help="Set to True to ingest only committees data",
-    )
-    return parser.parse_known_args()
-
-
 def get_keyword_args(keyword_args: list) -> dict:
     def parse_value(value: str) -> typing.Union[bool, str]:
         if value.lower() == "true":
@@ -103,13 +76,43 @@ def get_keyword_args(keyword_args: list) -> dict:
     return keyword_dict
 
 
-def main() -> int:
-    args, others = opts()
-    keyword_args_dict = get_keyword_args(others)
-    is_purge = args.purge or keyword_args_dict.get("purge", False)
-    force_ingest = args.force_ingest or keyword_args_dict.get("force-ingest", False)
-    people = args.people or keyword_args_dict.get("people", False)
-    committees = args.committees or keyword_args_dict.get("committees", False)
+@click.group()
+def main() -> None:
+    pass
+
+
+@main.command()
+@click.argument("other-options", nargs=-1)
+@click.option(
+    "--purge/--no-purge",
+    default=False,
+    help="Set to True to purge old data from database",
+)
+@click.option(
+    "--force-ingest/--no-force-ingest",
+    default=False,
+    help="Operate in safe mode, no changes will be written to database.",
+)
+@click.option(
+    "--people/--no-people",
+    default=False,
+    help="Operate in safe mode, no changes will be written to database.",
+)
+@click.option(
+    "--committees/--no-committees",
+    default=False,
+    help="Operate in safe mode, no changes will be written to database.",
+)
+@click.pass_context
+def update(ctx, other_options: list[str], purge: bool, force_ingest: bool, people: bool, committees: bool) -> int:
+
+    keyword_args_dict = get_keyword_args(other_options)
+
+    abbreviation = keyword_args_dict.get("abbreviation", None)
+    is_purge = purge or keyword_args_dict.get("purge", False)
+    force_ingest = force_ingest or keyword_args_dict.get("force-ingest", False)
+    people = people or keyword_args_dict.get("people", False)
+    committees = committees or keyword_args_dict.get("committees", False)
 
     if not force_ingest:
         logger.info("Checking if an update is necessary")
@@ -122,28 +125,18 @@ def main() -> int:
     logger.info(
         "An update is necessary...\nBegin updating Openstates People to-database!"
     )
+
     clone_people_repo()
-    people_arguments = ["python", "-m", "openstates.cli.people", "to-database"]
-    committee_arguments = ["python", "-m", "openstates.cli.committees", "to-database"]
-    try:
-        if is_purge:
-            people_arguments.append("--purge")
-            committee_arguments.append("--purge")
-        if people and not committees:
-            result = subprocess.run(people_arguments, check=True)
-            logger.info(f"Status code {result.returncode} returned for people.")
-        elif committees and not people:
-            result = subprocess.run(committee_arguments, check=True)
-            logger.info(f"Status code {result.returncode} returned for people.")
-        else:
-            people_result = subprocess.run(people_arguments, check=True)
-            committees_result = subprocess.run(committee_arguments, check=True)
-            logger.info(f"Status code {people_result.returncode} returned for people.")
-            logger.info(
-                f"Status code {committees_result.returncode} returned for committees."
-            )
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Subprocess failed with error: {e}")
+
+    abbreviations = [abbreviation] if abbreviation else []
+
+    if people and not committees:
+        ctx.invoke(people_to_database, abbreviations=abbreviations, purge=purge)
+    elif committees and not people:
+        ctx.invoke(committee_to_database, abbreviations=abbreviations, purge=purge)
+    else:
+        ctx.invoke(people_to_database, abbreviations=abbreviations, purge=purge)
+        ctx.invoke(committee_to_database, abbreviations=abbreviations, purge=purge)
     return 0
 
 
