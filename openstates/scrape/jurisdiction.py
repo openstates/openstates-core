817c68b01f59b8a09ec7773b692a581f145323cb
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2025-03-19 12:47:13 -0400

    serialized the scraped_name field from cronos

--- openstates/scrape/jurisdiction.py
+++ openstates/scrape/jurisdiction.py
@@ -124,6 +124,7 @@ class State(BaseModel):
                         "name",
                         "start_date",
                         "end_date",
+                        "_scraped_name"
                     ]
                 }
             )
