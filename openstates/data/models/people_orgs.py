cfaf407cd1066c0f265f64ac04897fe5c8371d8b
mirillazhu <mira.zhu@gmail.com>
2025-11-30 18:36:08 -0500

    Fixed duplicate legislators appearing in state-specific bill and legislator search

--- openstates/data/models/people_orgs.py
+++ openstates/data/models/people_orgs.py
@@ -156,7 +156,7 @@ class PersonQuerySet(QuerySet):
         if state:
             people = people.filter(
                 memberships__organization__jurisdiction_id=abbr_to_jid(state)
-            )
+            ).distinct()
         return people
 
 
